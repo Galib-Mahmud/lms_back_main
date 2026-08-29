7a1c3a69e4a82d36fa9009aa4769694d229428ad
Galib-Mahmud <galibmahmud.ducse@gmail.com>
2026-08-29 11:28:14 +0600

    Fix Railway deployment build start script and server port binding

--- config/server.js
+++ config/server.js
@@ -1,9 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://localhost:1337'),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['9x8A2bK1vN7mL4qR', '5tY8uI3oP0aS6dF1']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
