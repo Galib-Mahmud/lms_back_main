84429f02c6593ac6e3afb9acc95ef3fc4ffc9109
Galib-Mahmud <galibmahmud.ducse@gmail.com>
2026-08-29 13:57:28 +0600

    Use full UID handler notation for custom-auth routes

--- src/api/custom-auth/routes/custom-auth.js
+++ src/api/custom-auth/routes/custom-auth.js
@@ -5,7 +5,7 @@ module.exports = {
     {
       method: 'POST',
       path: '/custom-auth/register',
-      handler: 'custom-auth.register',
+      handler: 'api::custom-auth.custom-auth.register',
       config: {
         auth: false,
         policies: [],
@@ -14,7 +14,7 @@ module.exports = {
     {
       method: 'GET',
       path: '/custom-auth/me',
-      handler: 'custom-auth.me',
+      handler: 'api::custom-auth.custom-auth.me',
       config: {
         auth: false,
         policies: [],
